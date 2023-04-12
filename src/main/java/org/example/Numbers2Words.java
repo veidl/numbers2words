872f7566eb8b1f05dc593731d09e3a3320b304bd
veidl <thomas.polzer@protonmail.com>
2023-04-12 13:11:47 +0200

    impl: return NA for negative input

--- src/main/java/org/example/Numbers2Words.java
+++ src/main/java/org/example/Numbers2Words.java
@@ -3,6 +3,9 @@ package org.example;
 public class Numbers2Words {
 
     public String convert(long number) {
+        if (number < 0) {
+            return "N/A";
+        }
         return String.valueOf(number);
     }
 
