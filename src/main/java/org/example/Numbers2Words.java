6443d23626b67f47d87c09dc505d1d819052ad0c
veidl <thomas.polzer@protonmail.com>
2023-04-12 13:14:12 +0200

    impl: return number to large for to large number

--- src/main/java/org/example/Numbers2Words.java
+++ src/main/java/org/example/Numbers2Words.java
@@ -2,10 +2,17 @@ package org.example;
 
 public class Numbers2Words {
 
+    private final long MAX_VALUE = 999999999;
+
     public String convert(long number) {
         if (number < 0) {
             return "N/A";
         }
+
+        if(number > MAX_VALUE) {
+            return "Number to large";
+        }
+
         return String.valueOf(number);
     }
 
