9219c1ae14f270bf93866dd329b216e5c5dcaf9a
veidl <thomas.polzer@protonmail.com>
2023-04-12 13:29:32 +0200

    impl: impl converter for 745

--- src/main/java/org/example/Numbers2Words.java
+++ src/main/java/org/example/Numbers2Words.java
@@ -1,19 +1,48 @@
 package org.example;
 
 public class Numbers2Words {
-
     private final long MAX_VALUE = 999999999;
+    private final String[] ones = {"", "one", "two", "three", "four", "five ", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+    private final String[] tens = {"", "", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+    private final String[] thousands = {"", "thousand", "million", "billion", "trillion"};
 
     public String convert(long number) {
         if (number < 0) {
             return "N/A";
         }
 
-        if(number > MAX_VALUE) {
+        if (number > MAX_VALUE) {
             return "Number to large";
         }
 
-        return String.valueOf(number);
+        String words = "";
+        int i = 0;
+        while (number > 0) {
+            if (number % 1000 != 0) {
+                words = convertHundreds((int) (number % 1000)) + thousands[i] + " " + words;
+            }
+            number /= 1000;
+            i++;
+        }
+        return words.trim();
+    }
+
+    private String convertHundreds(int number) {
+        String current;
+        if (number % 100 < 20) {
+            current = ones[number % 100];
+            number /= 100;
+        } else {
+            current = ones[number % 10];
+            number /= 10;
+
+            current = tens[number % 10] + " " + current;
+            number /= 10;
+        }
+        if (number == 0) {
+            return current;
+        }
+        return ones[number] + " hundred and " + current;
     }
 
 }
--- src/test/java/org/example/Numbers2WordsTest.java
+++ src/test/java/org/example/Numbers2WordsTest.java
@@ -9,14 +9,6 @@ class Numbers2WordsTest {
     private final Numbers2Words numbers2Words = new Numbers2Words();
 
     @Test
-    void should_convert_long_to_string() {
-        String convert = numbers2Words.convert(33232);
-
-        assertEquals("33232", convert);
-
-    }
-
-    @Test
     void should_return_NA_if_number_is_negative() {
         assertEquals("N/A", numbers2Words.convert(-123));
     }
@@ -28,7 +20,7 @@ class Numbers2WordsTest {
 
     @Test
     void should_convert_number_to_words() {
-        assertEquals("even hundred and fourty five", numbers2Words.convert(745));
+        assertEquals("seven hundred and fourty five", numbers2Words.convert(745));
     }
 
 }
